1f1c246417dbcc59d700d4b854ca4d4d05e5b04f
paulosarmento <paulo.sarmentojunior@alunos.unis.edu.br>
2020-08-03 02:13:51 -0300

    Criando módulos

---
+++ modules/module-01.js
@@ -0,0 +1,10 @@
+console.log('Executando Module-01.js')
+
+module.exports.oculta = ()=>{
+    console.log('Executando função Oculta')
+}
+module.exports.executa = ()=>{
+    console.log('Executando a função executa')
+}
+
+module.exports.welcome = `Bem Vindo ao módulo module-02.js`
